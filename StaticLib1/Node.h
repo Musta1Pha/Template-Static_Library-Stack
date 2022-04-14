bff426c00e4e64becb9cbfae81f7c2e2e99e8e53
Mustapha Khlil <91842692+Musta1Pha@users.noreply.github.com>
2022-04-14 01:21:55 +0000

    Add files via upload

---
+++ StaticLib1/Node.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Node
+{
+public:
+	Node(T data);
+	void Afficher() const;
+	Node<T>* GetNode();
+	void SetNode(Node<T>* n);
+	~Node();
+	friend class Stack;
+private:
+	T Data;
+	Node<T>* Next;
+
+};
+
+template <class T>
+Node<T>::Node(T Data)
+{
+	this->Data = Data;
+	this->Next = nullptr;
+}
+
+template <class T>
+void Node<T>::Afficher()const
+{
+	cout << this->Data << endl;
+}
+
+template <class T>
+Node<T>* Node<T>::GetNode()
+{
+	return this->Next;
+}
+
+template <class T>
+void Node<T>::SetNode(Node<T>* n)
+{
+	this->Next = n;
+}
+
+template <class T>
+Node<T>::~Node()
+{
+	delete this->Next;
+	this->Next = 0;
+}
